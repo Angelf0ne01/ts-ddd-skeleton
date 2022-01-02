17f13c5fb6c257b938e84c6280bda400e61b22a0
Angel Vazquez <angel.vazquez@extendeal.com>
2022-01-02 12:36:57 -0300

    add user creator

--- src/apps/mooc/backend/controllers/UserPutController.ts
+++ src/apps/mooc/backend/controllers/UserPutController.ts
@@ -3,13 +3,13 @@ import httpStatus from 'http-status';
 import { UserCreator } from '../../../../Contexts/Mooc/Users/application/UserCreator';
 import { Controller } from './Controller';
 
-export class CoursePutController implements Controller {
-  constructor(private courseCreator: UserCreator) {}
+export class UserPutController implements Controller {
+  constructor(private userCreator: UserCreator) {}
 
   async run(req: Request, res: Response) {
     const { id, first_name, last_name } = req.body;
 
-    await this.courseCreator.run({ id, first_name, last_name });
+    await this.userCreator.run({ id, first_name, last_name });
 
     res.status(httpStatus.CREATED).send();
   }
